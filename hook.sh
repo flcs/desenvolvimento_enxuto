93eb9cf2e78733d0f0f76604a3c7f837f5f04718
Fernando Carvalho <fernando.carvalho.iff@gmail.com>
2012-02-29 16:02:51 -0300

    Update hook.sh

--- hook.sh
+++ hook.sh
@@ -1,8 +1,7 @@
 
-
 set -x
 
-curl -X POST -u "flcs:aniram" -i \
+curl -X POST -u "flcs:xxxxxxxx" -i \
   https://api.github.com/repos/flcs/desenvolvimento_enxuto/hooks \
   -d '{ "name": "web", "active": true, "config": { "url": "http://flcs.homeunix.com:8080/webhook/desenvolvimento_enxuto" } } '
 
